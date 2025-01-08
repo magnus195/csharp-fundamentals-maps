9e38f5fc34bd84081fef5b94832a532f82a071f0
magnus195 <22998711+magnus195@users.noreply.github.com>
2025-01-08 11:26:30 +0100

    Removed redundant imports

--- csharp-fundamentals-maps.Main/Core.cs
+++ csharp-fundamentals-maps.Main/Core.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace csharp_fundamentals_maps.Main
+﻿namespace csharp_fundamentals_maps.Main
 {
      public class Core
      {
--- csharp-fundamentals-maps.Main/Extension.cs
+++ csharp-fundamentals-maps.Main/Extension.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace csharp_fundamentals_maps.Main
+﻿namespace csharp_fundamentals_maps.Main
 {
     public  class Extension
     {
